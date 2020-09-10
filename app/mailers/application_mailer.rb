0325b75d460f0d9e91177cd849326e52579bce22
JordanT <jo.tournerie@gmail.com>
2020-09-10 11:34:56 +0200

    mailer improved

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@tousaumarche.com'
+  default from: 'jo.tournerie@gmail.com'
   layout 'bootstrap-mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'jo.tournerie@gmail.com'
 
   def welcome_email(user)
-    #we get the user instance so we can pass it on to the view via @user
     @user = user
-
-    #we define an @url variable that we'll use in the e-mail view
     @url  = 'https://allons-tous-au-marche.herokuapp.com/login'
-
-    #it is this call to mail() that allows us to send the e-mail by defining recipient and subject.
     make_bootstrap_mail(to: @user.email, subject: 'Bienvenue sur votre espace utilisateur TOUS AU MARCHE !')
   end
 
