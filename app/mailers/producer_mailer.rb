57189e5e83532faebfa3dddf12d8cf484937572e
JordanT <jo.tournerie@gmail.com>
2020-09-10 17:09:37 +0200

    try to put on mail on password change

--- app/mailers/producer_mailer.rb
+++ app/mailers/producer_mailer.rb
@@ -2,16 +2,14 @@ class ProducerMailer < ApplicationMailer
   default from: 'jo.tournerie@gmail.com'
 
   def welcome_email(producer)
-    #we get the user instance so we can pass it on to the view via @user
     @producer = producer
-    #we define an @url variable that we'll use in the e-mail view
-    @url  = 'https://allons-tous-au-marche.herokuapp.com/login'
-    make_bootstrap_mail(to: @producer.email, subject: 'Bienvenue sur votre espace producteur TOUS AU MARCHE !')
+    @url  = 'https://allons-tous-au-marche.herokuapp.com/'
+    make_bootstrap_mail(to: @producer.email, subject: 'Bienvenue chez TOUS AU MARCHE !')
   end
 
   def goodbye_email(producer)
     @producer = producer
-    @url  = 'https://allons-tous-au-marche.herokuapp.com' 
+    @url  = 'https://allons-tous-au-marche.herokuapp.com/producers/sign_up'
     make_bootstrap_mail(to: @producer.email, subject: 'On se revoit bientôt ?')
   end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #### Mailer ####
+  config.action_mailer.default_url_options = { :host => 'allons-tous-au-marche.herokuapp.com' }
+  #### end of Mailer ####
+
 end
