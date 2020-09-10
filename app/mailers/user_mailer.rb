da16261c0523b403180b5f2351f54fe8c37b9156
JordanT <jo.tournerie@gmail.com>
2020-09-10 12:08:04 +0200

    mailer improved AND for heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url  = 'https://allons-tous-au-marche.herokuapp.com/login'
-    make_bootstrap_mail(to: @user.email, subject: 'Bienvenue sur votre espace utilisateur TOUS AU MARCHE !')
+    @url  = 'https://allons-tous-au-marche.herokuapp.com/'
+    make_bootstrap_mail(to: @user.email, subject: 'Bienvenue chez TOUS AU MARCHE !')
   end
 
   def goodbye_email(user)
     @user = user
-    @url  = 'https://allons-tous-au-marche.herokuapp.com'
+    @url  = 'https://allons-tous-au-marche.herokuapp.com/users/sign_up'
     make_bootstrap_mail(to: @user.email, subject: 'On se revoit bientôt ?')
   end
 
